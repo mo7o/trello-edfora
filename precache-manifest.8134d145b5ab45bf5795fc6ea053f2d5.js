13cadbe91cf85242ed08b64e6c968209aa8919f1
Mohit Mojo <mohit.mkj06@gmail.com>
2019-10-15 00:27:02 +0530

    trello build prod

---
+++ precache-manifest.8134d145b5ab45bf5795fc6ea053f2d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c7a6c2a2a1ae19459ca8ee90919ddf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6b136e29cca173bd6ca",
+    "url": "/static/css/main.273e1e2f.chunk.css"
+  },
+  {
+    "revision": "2ccf665580097432889d",
+    "url": "/static/js/2.bfca7a72.chunk.js"
+  },
+  {
+    "revision": "b6b136e29cca173bd6ca",
+    "url": "/static/js/main.a1b2eca9.chunk.js"
+  },
+  {
+    "revision": "9cf0b77287d19e822096",
+    "url": "/static/js/runtime-main.89c9cfde.js"
+  }
+]);
